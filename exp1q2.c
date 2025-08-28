b2285599c483dcb7224cd28257cd9b9f052bc637
Priyanshu Pabari <priyanshupabari007@gmail.com>
2025-08-28 22:00:35 +0530

    Add files via upload

---
+++ exp1q2.c
@@ -0,0 +1,10 @@
+//2.	Write a C Program to print the address in multiple lines (new line).
+
+
+#include <stdio.h>
+int main()
+{
+  printf("OSTELLO\n UPES OFFCAMPUS\n BOYS HOSTEL\n");
+
+  return 0;
+}
