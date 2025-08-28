15b84f6874f80a0a72b38985e4fa99c945911868
Priyanshu Pabari <priyanshupabari007@gmail.com>
2025-08-28 23:48:38 +0530

    Add files via upload

---
+++ exp1q4.c
@@ -0,0 +1,15 @@
+// 4.	Write a C program to add two numbers, take number from user.
+
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter a integer ");
+    scanf("%d", &a);
+    printf("enter a integer ");
+    scanf("%d", &b);
+    int c;
+    c = a + b;
+    printf("the sum is %d", c);
+    return 0;
+}
